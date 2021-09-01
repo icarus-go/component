85a43903554820e06a44acda391955ad901f2b9d
qingshen.cai <qingshen.cai@yz-intelligence.com>
2021-09-01 16:51:27 +0800

    refactor: - ID设置为主键

--- db/repo/imodel.go
+++ db/repo/imodel.go
@@ -6,15 +6,15 @@ import (
 )
 
 type S struct {
-	ID        string         `json:"id" gorm:"id" example:"ID"` // ID
-	CreatedAt time.Time      `json:"createdAt,omitempty"`       // 创建时间
-	UpdatedAt time.Time      `json:"updatedAt,omitempty"`       // 更新数据
-	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`            // 删除时间
+	ID        string         `json:"id" gorm:"id,primary" example:"ID"` // ID
+	CreatedAt time.Time      `json:"createdAt,omitempty"`               // 创建时间
+	UpdatedAt time.Time      `json:"updatedAt,omitempty"`               // 更新数据
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`                    // 删除时间
 }
 
 type M struct {
-	ID        uint64         `json:"id" gorm:"id" example:"1"` // ID
-	CreatedAt time.Time      `json:"createdAt,omitempty"`      // 创建时间
-	UpdatedAt time.Time      `json:"updatedAt,omitempty"`      // 更新时间
-	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`           // 删除时间
+	ID        uint64         `json:"id" gorm:"id,primary" example:"1"` // ID
+	CreatedAt time.Time      `json:"createdAt,omitempty"`              // 创建时间
+	UpdatedAt time.Time      `json:"updatedAt,omitempty"`              // 更新时间
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`                   // 删除时间
 }
