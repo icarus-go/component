656ca9160bdd3e9e940c83b9166f2e101ac33c0b
qingshen.cai <qingshen.cai@yz-intelligence.com>
2021-09-13 23:33:20 +0800

    refactor:
    - struct change

--- gins/request/query.go
+++ gins/request/query.go
@@ -2,24 +2,27 @@ package request
 
 import "errors"
 
-type Query struct {
+type (
+	Query struct {
+		String  String  `json:"string"`  // 字符串ID
+		Integer Integer `json:"integer"` // 整形ID
+		Paging
+	}
 	String struct {
 		ID  string   `json:"id" example:"ID" v:"required|length:1,1000#请输入id|id长度为:min到:max位"` // ID,当前业务主键
 		IDs []string `json:"ids" example:"ID组"`                                                // ID串组
-	} `json:"string"`
+	}
 
 	Integer struct {
 		ID  uint64   `json:"id" example:"1"` // ID
 		IDs []uint64 `json:"ids"`            // ID串组
-	} `json:"integer"` // 整形ID
+	}
 
-	Paging
-}
-
-type Paging struct {
-	Page     int `json:"page" form:"page" example:"1"`          // 页码
-	PageSize int `json:"pageSize" form:"pageSize" example:"20"` // 页面最大条数
-}
+	Paging struct {
+		Page     int `json:"page" form:"page" example:"1"`          // 页码
+		PageSize int `json:"pageSize" form:"pageSize" example:"20"` // 页面最大条数
+	}
+)
 
 //ResetPage 重置
 func (q *Query) ResetPage() *Query {
@@ -32,29 +35,29 @@ func (q *Query) ResetPage() *Query {
 	return q
 }
 
-func (q *Query) HasIntegerID() error {
-	if q.Integer.ID == 0 {
+func (q *Integer) HasID() error {
+	if q.ID == 0 {
 		return errors.New("ID不允许为0")
 	}
 	return nil
 }
 
-func (q *Query) HasIntegerIDs() error {
-	if len(q.Integer.IDs) < 1 {
+func (q *Integer) HasIDs() error {
+	if len(q.IDs) < 1 {
 		return errors.New("IDs不允许为空")
 	}
 	return nil
 }
 
-func (q *Query) HasStringIDValue() error {
-	if q.String.ID == "" {
+func (q *String) HasID() error {
+	if q.ID == "" {
 		return errors.New("ID不允许为空")
 	}
 	return nil
 }
 
-func (q *Query) HasStringIDsValue() error {
-	if len(q.String.IDs) < 1 {
+func (q *String) HasIDs() error {
+	if len(q.IDs) < 1 {
 		return errors.New("ids长度必须大于0")
 	}
 	return nil
