03a9799fa4f007e023c1a54dcc136c1ceb39a1ed
qingshen.cai <qingshen.cai@yz-intelligence.com>
2021-09-29 04:21:27 +0800

    refactor:
    - swagger 声明为 integer

--- gins/result/api_reuslt.go
+++ gins/result/api_reuslt.go
@@ -3,7 +3,7 @@ package result
 import "pmo-test4.yz-intelligence.com/kit/component/apiconstant"
 
 type ApiResult struct {
-	Code   apiconstant.ResponseType `json:"code"`
+	Code   apiconstant.ResponseType `json:"code" swaggertype:"integer"`
 	Msg    string                   `json:"msg"`
 	Data   interface{}              `json:"data"`
 	DataKV map[string]interface{}   `json:"-"`
