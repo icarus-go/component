f81f6edf23b8d7239c1a19a90acdbf7ce3fdc820
qingshen.cai <qingshen.cai@yz-intelligence.com>
2021-09-29 18:10:24 +0800

    refactor:
    - deletedAt swaggerignore: true

--- db/repo/imodel.go
+++ db/repo/imodel.go
@@ -10,12 +10,12 @@ type S struct {
 	ID        string         `json:"id" gorm:"column:id;size:32;primaryKey" swaggertype:"string" example:"uint64 主键ID"` // ID
 	CreatedAt time.Time      `json:"createdAt,omitempty" swaggertype:"string" example:"创建时间"`                           // 创建时间
 	UpdatedAt time.Time      `json:"updatedAt,omitempty" swaggertype:"string" example:"更新时间"`                           // 更新时间
-	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`                                                                    // 删除时间
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index" swaggerignore:"true"`                                               // 删除时间
 }
 
 type M struct {
 	ID        uint64         `json:"id" gorm:"column:id;size:20;primaryKey" swaggertype:"string" example:"uint64 主键ID"` // ID
 	CreatedAt time.Time      `json:"createdAt,omitempty" swaggertype:"string" example:"创建时间"`                           // 创建时间
 	UpdatedAt time.Time      `json:"updatedAt,omitempty" swaggertype:"string" example:"更新时间"`                           // 更新时间
-	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`                                                                    // 删除时间
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index" swaggerignore:"true"`                                               // 删除时间
 }
