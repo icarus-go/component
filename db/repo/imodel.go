800e4851cd6f5cfb02fd0e4e8b8d202a45c1f8e0
SliverHorn <sliver_horn@qq.com>
2021-10-09 15:07:32 +0800

    refactor: time.Time => Datetime

--- db/repo/imodel.go
+++ db/repo/imodel.go
@@ -1,21 +1,17 @@
 package repo
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
+import "gorm.io/gorm"
 
 type S struct {
 	ID        string         `json:"id" gorm:"column:id;size:32;primaryKey" swaggertype:"string" example:"uint64 主键ID"` // ID
-	CreatedAt time.Time      `json:"createdAt,omitempty" swaggertype:"string" example:"创建时间"`                           // 创建时间
-	UpdatedAt time.Time      `json:"updatedAt,omitempty" swaggertype:"string" example:"更新时间"`                           // 更新时间
+	CreatedAt Datetime       `json:"createdAt,omitempty" swaggertype:"string" example:"创建时间"`                           // 创建时间
+	UpdatedAt Datetime       `json:"updatedAt,omitempty" swaggertype:"string" example:"更新时间"`                           // 更新时间
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index" swaggerignore:"true"`                                               // 删除时间
 }
 
 type M struct {
 	ID        uint64         `json:"id" gorm:"column:id;size:20;primaryKey" swaggertype:"string" example:"uint64 主键ID"` // ID
-	CreatedAt time.Time      `json:"createdAt,omitempty" swaggertype:"string" example:"创建时间"`                           // 创建时间
-	UpdatedAt time.Time      `json:"updatedAt,omitempty" swaggertype:"string" example:"更新时间"`                           // 更新时间
+	CreatedAt Datetime       `json:"createdAt,omitempty" swaggertype:"string" example:"创建时间"`                           // 创建时间
+	UpdatedAt Datetime       `json:"updatedAt,omitempty" swaggertype:"string" example:"更新时间"`                           // 更新时间
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index" swaggerignore:"true"`                                               // 删除时间
 }
