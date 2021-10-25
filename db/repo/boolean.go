9890ef9037d42f7501bd6cab2dd6267711691295
KevinCC <kevinicarus@qq.com>
2021-10-26 01:06:22 +0800

    fix:
    - 修复boolean反射失败的问题

--- db/repo/boolean.go
+++ db/repo/boolean.go
@@ -4,82 +4,51 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
-	"go.uber.org/zap"
 	"strconv"
 )
 
-type Boolean struct {
-	Result *bool
-}
+type Boolean bool
 
 // Int 布尔对应数值
 //  Author:  Kevin·CC
 func (b Boolean) Int() int {
-	isOK := 0
-	if b.Result != nil && *b.Result {
-		isOK = 1
+	if b {
+		return 1
 	}
-	return isOK
-}
-
-// NotNil 是否为空
-//  Author:  Kevin·CC
-func (b *Boolean) NotNil() bool {
-	return b.Result != nil
+	return 0
 }
 
 // Scan 扫描
-// Author SliverHorn
+// Author Kevin·CC
 func (b *Boolean) Scan(value interface{}) error {
 	nullBool := sql.NullBool{}
-	if err := nullBool.Scan(value); err != nil {
-
-		nullInt := sql.NullInt32{}
-
-		if err := nullInt.Scan(value); err != nil {
-			zap.L().Error("Boolean To Database 时间转换失败!", zap.Any("datetime", value))
-			return err
-		}
-
-		if nullInt.Int32 != 0 {
-			result := true
-			b.Result = &result
-			return nil
-		}
-		result := false
-		b.Result = &result
-	}
-	b.Result = &nullBool.Bool
-	return nil
+	err := nullBool.Scan(value)
+	*b = Boolean(nullBool.Bool)
+	return err
 }
 
 // Value 值
-// Author SliverHorn
+// Author Kevin·CC
 func (b *Boolean) Value() (driver.Value, error) {
-	return driver.Value(strconv.FormatBool(*b.Result)), nil
+	return driver.Value(strconv.FormatBool(bool(*b))), nil
 }
 
 // MarshalJSON 序列化
-// Author SliverHorn
+// Author Kevin·CC
 func (b *Boolean) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf(`"%s"`, strconv.FormatBool(*b.Result))), nil
+	return []byte(fmt.Sprintf(`"%s"`, strconv.FormatBool(bool(*b)))), nil
 }
 
 // UnmarshalJSON 反序列化
-// Author SliverHorn
+// Author Kevin·CC
 func (b *Boolean) UnmarshalJSON(bytes []byte) error {
 	boolValue, err := strconv.ParseBool(string(bytes))
-	if err != nil {
-		zap.L().Error("boolean parse fail!", zap.String("value", string(bytes)))
-		return err
-	}
-	b.Result = &boolValue
-
-	return nil
+	*b = Boolean(boolValue)
+	return err
 }
 
 // GormDataType gorm 定义数据库字段类型
-// Author SliverHorn
+// Author Kevin·CC
 func (b *Boolean) GormDataType() string {
 	return "tinyint"
 }
