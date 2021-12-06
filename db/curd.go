556017d925652c66165f429eaa77fc1f65e60230
Kevin-CC <kevinicarus@163.com>
2021-12-07 01:05:58 +0800

    refactor:
    - 重构CURL的配置

--- db/curd.go
+++ db/curd.go
@@ -8,8 +8,16 @@ import (
 )
 
 type CURL struct {
-	Gorm
-	Model schema.Tabler
+	instance *gorm.DB
+	model    schema.Tabler
+}
+
+// SetDBInstance
+//  Author: Kevin·CC
+//  Description: 设置数据库实例对象
+//  Param instance
+func (c *CURL) SetDBInstance(instance *gorm.DB) {
+	c.instance = instance
 }
 
 // Delete
@@ -19,12 +27,12 @@ type CURL struct {
 //  Param scopes 其他条件
 //  Param idColumnName id字段名称
 //  Return error 错误信息
-func (m *CURL) Delete(info params.IWhere, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
-	base := m.DB.Scopes(info.Scopes(idColumnName...))
+func (c *CURL) Delete(info params.IWhere, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
+	base := c.instance.Scopes(info.Scopes(idColumnName...))
 	if scopes != nil {
 		base.Scopes(scopes)
 	}
-	return base.Delete(m.Model).Error
+	return base.Delete(c.model).Error
 }
 
 // First
@@ -35,12 +43,12 @@ func (m *CURL) Delete(info params.IWhere, scopes func(*gorm.DB) *gorm.DB, idColu
 //  Param scopes 范围
 //  Param idColumnName 主键字段名称
 //  Return error 错误信息
-func (m *CURL) First(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
-	base := m.DB.Scopes(info.Scopes(idColumnName...))
+func (c *CURL) First(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
+	base := c.instance.Scopes(info.Scopes(idColumnName...))
 	if scopes != nil {
 		base.Scopes(scopes)
 	}
-	return base.First(&value).Error
+	return base.Model(c.model).First(&value).Error
 }
 
 // FirstOne
@@ -51,8 +59,8 @@ func (m *CURL) First(info params.IWhere, value interface{}, scopes func(*gorm.DB
 //  Param scopes 除ID外其他筛选
 //  Param idColumnName ID字段名称
 //  Return error 错误信息
-func (m *CURL) FirstOne(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
-	err := m.First(info, value, scopes, idColumnName...)
+func (c *CURL) FirstOne(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
+	err := c.First(info, value, scopes, idColumnName...)
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil
 	}
@@ -67,12 +75,12 @@ func (m *CURL) FirstOne(info params.IWhere, value interface{}, scopes func(*gorm
 //  Param scopes
 //  Param idColumnName
 //  Return error
-func (m *CURL) FindOne(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
-	base := m.DB.Scopes(info.Scopes(idColumnName...))
+func (c *CURL) FindOne(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
+	base := c.instance.Scopes(info.Scopes(idColumnName...))
 	if scopes != nil {
 		base.Scopes(scopes)
 	}
-	return base.Limit(1).Find(&value).Error
+	return base.Model(c.model).Limit(1).Find(&value).Error
 }
 
 // Find
@@ -83,8 +91,8 @@ func (m *CURL) FindOne(info params.IWhere, value interface{}, scopes func(*gorm.
 //  Param scopes 其他筛选
 //  Param idColumnName 主键ID名称
 //  Return error 错误信息
-func (m *CURL) Find(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
-	tx := m.DB.Scopes(info.Scopes(idColumnName...))
+func (c *CURL) Find(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
+	tx := c.instance.Scopes(info.Scopes(idColumnName...))
 	if scopes != nil {
 		tx.Scopes(scopes)
 	}
@@ -99,10 +107,10 @@ func (m *CURL) Find(info params.IWhere, value interface{}, scopes func(*gorm.DB)
 //  Param scopes 更多的筛选条件
 //  Param idColumnName ID字段名称
 //  Return error 错误信息
-func (m *CURL) Update(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
-	base := m.DB.Scopes(info.Scopes(idColumnName...))
+func (c *CURL) Update(info params.IWhere, value interface{}, scopes func(*gorm.DB) *gorm.DB, idColumnName ...string) error {
+	base := c.instance.Scopes(info.Scopes(idColumnName...))
 	if scopes != nil {
 		base.Scopes(scopes)
 	}
-	return base.Updates(&value).Error
+	return base.Model(c.model).Updates(&value).Error
 }
